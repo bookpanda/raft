6b15dbccc8491e6d4a72d86c7525405e939b778d
Idhibhat Pankam <marginpankam@gmail.com>
2025-07-13 14:21:45 +0700

    datastore

---
+++ src/kv/kvservice/datastore.go
@@ -0,0 +1,42 @@
+package kvservice
+
+import "sync"
+
+type DataStore struct {
+	sync.Mutex
+	data map[string]string
+}
+
+func NewDataStore() *DataStore {
+	return &DataStore{
+		data: make(map[string]string),
+	}
+}
+
+func (ds *DataStore) Get(key string) (string, bool) {
+	ds.Lock()
+	defer ds.Unlock()
+
+	value, ok := ds.data[key]
+	return value, ok
+}
+
+func (ds *DataStore) Put(key, value string) (string, bool) {
+	ds.Lock()
+	defer ds.Unlock()
+
+	v, ok := ds.data[key]
+	ds.data[key] = value
+	return v, ok
+}
+
+func (ds *DataStore) CAS(key, compare, value string) (string, bool) {
+	ds.Lock()
+	defer ds.Unlock()
+
+	v, ok := ds.data[key]
+	if ok && v == compare {
+		ds.data[key] = value
+	}
+	return v, ok
+}
